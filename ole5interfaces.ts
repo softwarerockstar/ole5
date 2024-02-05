252eb1f6367a03729023d154bfcb6e91b3c22a74
Software Rockstar <mharoon@softwarerockstar.com>
2024-02-05 16:02:51 -0600

    up to line 145

--- ole5interfaces.ts
+++ ole5interfaces.ts
@@ -1,39 +1,54 @@
+export interface OleStep5RateInterface {
+    Description?: string //coverage.Description
+    EffectiveDate?: string //coverage.Rates.EffectiveDate
+    ExpirationDate?: string //coverage.Rates.ExpirationDate
+    TotalAmount?: number //coverage.Rates.TotalAmount
+}
+
+
 export interface OleStep5CoverageInterface {
-    Description: string
+    Description?: string //coverage.Description
+    Rates: OleStep5RateInterface[]
 }
 export interface OleStep5DataInterface {
     isControl: boolean //yearCtrl.isControl
-    policyType: string //yearCtrl.policyType
-    policyName: string //yearCtrl.sessionInfo.isCon?yearCtrl.conTxt:yearCtrl.policyName
-    policyNumber: string //yearCtrl.policyNumber
-    underwriterCompany: string //yearCtrl.uwCompany
-    ole5Title: string //schoolCtrl.ole5Title
-    ole5Content1: string //schoolCtrl.ole5Content1
-    ole5Content2: string //schoolCtrl.ole5Content2
+    policyType?: string //yearCtrl.policyType
+    policyName?: string //yearCtrl.sessionInfo.isCon?yearCtrl.conTxt:yearCtrl.policyName
+    policyNumber?: string //yearCtrl.policyNumber
+    underwriterCompany?: string //yearCtrl.uwCompany
+    ole5Title?: string //schoolCtrl.ole5Title
+    ole5Content1?: string //schoolCtrl.ole5Content1
+    ole5Content2?: string //schoolCtrl.ole5Content2
     creditDecline: boolean //yearCtrl.creditDecline
     
     recentCoverage: boolean //yearCtrl.recentCoverage
-    recentPolicyNumber: string //yearCtrl.recent.policyNumber
-    recentSchoolName: string //yearCtrl.recent.schoolName
-    recentCampus: string //yearCtrl.recent.campus
-    recentProductName: string //yearCtrl.recent.productName
+    recentPolicyNumber?: string //yearCtrl.recent.policyNumber
+    recentSchoolName?: string //yearCtrl.recent.schoolName
+    recentCampus?: string //yearCtrl.recent.campus
+    recentProductName?: string //yearCtrl.recent.productName
     recentCoverageType: boolean //yearCtrl.underwriteratTopAllowedStates.indexOf(yearCtrl.regulatoryStateCode)>-1 && yearCtrl.recent.coverageType === 'Spouse' ? 'Spouse [or Domestic Partner]' : yearCtrl.recent.coverageType
-    recentEffectiveDate: string //yearCtrl.recent.effectiveDate
-    recentExpirationDate: string //yearCtrl.recent.expirationDate
+    recentEffectiveDate?: string //yearCtrl.recent.effectiveDate
+    recentExpirationDate?: string //yearCtrl.recent.expirationDate
 
-    school: string //yearCtrl.school
-    productName: string //yearCtrl.sessionInfo.isCon?yearCtrl.conTxt:yearCtrl.policyName
+    school?: string //yearCtrl.school
+    productName?: string //yearCtrl.sessionInfo.isCon?yearCtrl.conTxt:yearCtrl.policyName
 
     // Line 75-76
 
-    enrollmentEffectiveDate: string //yearCtrl.enrollmentInfo.effectiveDate
-    enrollmentExpirationDate: string //yearCtrl.enrollmentInfo.expirationDate
-    enrollmentRateSelectedPeriodCode: string //enrollmentInfo.rateSelected.periodCode
-    enrollmentRateSelectedEffectiveDate: string //yearCtrl.enrollmentInfo.rateSelected.effectiveDate
-    enrollmentRateSelectedExpirationDate: string //yearCtrl.enrollmentInfo.rateSelected.expirationDate
+    enrollmentEffectiveDate?: string //yearCtrl.enrollmentInfo.effectiveDate
+    enrollmentExpirationDate?: string //yearCtrl.enrollmentInfo.expirationDate
+    enrollmentRateSelectedPeriodCode?: string //enrollmentInfo.rateSelected.periodCode
+    enrollmentRateSelectedEffectiveDate?: string //yearCtrl.enrollmentInfo.rateSelected.effectiveDate
+    enrollmentRateSelectedExpirationDate?: string //yearCtrl.enrollmentInfo.rateSelected.expirationDate
+    enrollmentRateSelectedPartialPaymentEffectiveDate?: string //enrollmentInfo.rateSelected.partialPaymentDates[0].EffectiveDate
+    enrollmentRateSelectedPartialPaymentExpirationDate?: string//enrollmentInfo.rateSelected.partialPaymentDates[0].ExpirationDate
     
 
     coverages: OleStep5CoverageInterface[]; //coverage in yearCtrl.coverages
+
+    // Line 107
+
+    // On line 145
 
 
 
